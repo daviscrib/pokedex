68f497144b09349c8693a327515735f17db7fa52
David L <159317073+daviscrib@users.noreply.github.com>
2024-04-02 21:02:44 +0200

    props.1

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,23 @@ import CardList from "./components/CardList";
 import PokemonCard from "./components/PokemonCard";
 import Footer from "./components/Footer";
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 function App() {
     return (
     <>
         <Header/>
         <CardList/>
-        <PokemonCard/>  
+        <PokemonCard pokemon = {pokemonList[0]} />  
         <Footer/>
     </>
   )
